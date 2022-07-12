7ad78c7ee039152fe209d81d0312ed7fdc955856
Lucas Bernardes <lucasbernardes24@gmail.com>
2022-07-12 14:54:33 -0300

    findById implementation

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,19 +1,15 @@
 package app;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
-    public static void main(String[] args) {
-        Department dp = new Department(1, "Computers");
-        Seller sl = new Seller(33, "Lucas", "lucas@gmail.com", new Date(), 4500.0, dp);
-        
+    public static void main(String[] args) {       
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        
-        System.out.println(sl);
+
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import model.dao.SellerDao;
+import model.db.DB;
+import model.db.DbException;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
+
+    private Connection con;
+
+    public SellerDaoJDBC(Connection con){
+        this.con = con;
+    }
 
     @Override
     public void insert(Seller dp) {
@@ -27,8 +40,35 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-        // TODO Auto-generated method stub
-        return null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+
+        try{
+            pst = con.prepareStatement(
+            "SELECT seller.*,department.Name as DepName "
+            + "FROM seller INNER JOIN department "
+            + "ON seller.DepartmentId = department.Id "
+            + "WHERE seller.Id = ?");
+
+            pst.setInt(1, id);
+            rs = pst.executeQuery();
+            
+            if(rs.next()){
+                Department dp = new Department(rs.getInt("DepartmentId"), rs.getString("DepName"));
+                Seller sl = new Seller(rs.getInt("Id"), rs.getString("Name"), rs.getString("Email"), rs.getDate("BirthDate"), rs.getDouble("BaseSalary"), dp);
+                return sl;
+            }
+            
+            return null;
+        }
+        catch(SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally{
+            DB.closeStatement(pst);
+            DB.closeResultSet(rs);
+        }
+        
     }
 
     @Override
